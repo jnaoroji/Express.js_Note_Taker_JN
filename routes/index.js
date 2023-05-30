6841acfbf4dc28ed94c498a1272dbbf6c8a262ba
jnaoroji <jennynaoroji@gmail.com>
2023-05-30 13:52:15 +1000

    node is able to run application on local server

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 //import all dependencies
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
-const uuid = require('./helpers/uuid');
-//import middlewear
 const { clog } = require('./middleware/clog');
-const api = require('./public/assets/js/index');
+const api = require('./routes/index.js');
+const fs = require('fs');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -24,24 +22,24 @@ app.use(express.static('public'));
 
 // GET HTML routes for homepage
 app.get('/api/notes', (req, res) => {
-  const notesData = fs.readFileSync('db.json', 'utf8');
+  const notesData = fs.readFileSync('db/db.json', 'utf8');
   const notes = JSON.parse(notesData);
   res.json(notes);
 });
 
-// GET Route for notes page
+// GET Route for index page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// POST Route for disgnostics page
+// POST Route for notes page
 app.post('/api/notes', (req, res) => {
-  const notesData = fs.readFileSync('db.json', 'utf8');
+  const notesData = fs.readFileSync('db/db.json', 'utf8');
   const notes = JSON.parse(notesData);
   const newNote = req.body;
   newNote.id = uuid(); // You can use a package to generate a unique ID
   notes.push(newNote);
-  fs.writeFileSync('db.json', JSON.stringify(notes));
+  fs.writeFileSync('db/db.json', JSON.stringify(notes));
   res.json(newNote);
 });
 
