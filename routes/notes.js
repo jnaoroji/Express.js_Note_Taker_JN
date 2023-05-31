1e13ec48b511e4882f650a61ef5a170bce82f601
jnaoroji <jennynaoroji@gmail.com>
2023-05-31 12:20:51 +1000

    get,post and delete paths created

--- routes/notes.js
+++ routes/notes.js
@@ -7,31 +7,19 @@ const {
 } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-notes.get('/', (req, res) => {
+notes.get('/notes', (req, res) => {
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// GET Route for a specific note
-notes.get('/:note_id', (req, res) => {
-  const noteId = req.params.note_id;
-  readFromFile('./db/db.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.note_id === noteId);
-      return result.length > 0
-        ? res.json(result)
-        : res.json('No note with that ID');
-    });
-});
 
-// DELETE Route for a specific note
+// DELETE Route for a specific tip
 notes.delete('/:note_id', (req, res) => {
   const noteId = req.params.note_id;
   readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
       // Make a new array of all notes except the one with the ID provided in the URL
-      const result = json.filter((note) => note.note_id !== noteId);
+      const result = json.filter((note) => notes.note_id !== noteId);
 
       // Save that array to the filesystem
       writeToFile('./db/db.json', result);
@@ -41,22 +29,21 @@ notes.delete('/:note_id', (req, res) => {
     });
 });
 
-// POST Route for a new UX/UI note
+// POST Route for a new UX/UI tip
 notes.post('/', (req, res) => {
   console.log(req.body);
 
-  const { username, topic, note } = req.body;
+  const { noteTitle, noteText } = req.body;
 
   if (req.body) {
-    const newnote = {
-      username,
-      note,
-      topic,
+    const newNote = {
+      noteTitle,
+      noteText,
       note_id: uuidv4(),
     };
 
-    readAndAppend(newnote, './db/db.json');
-    res.json(`note added successfully 🚀`);
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully!`);
   } else {
     res.error('Error in adding note');
   }
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-//import all dependencies
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
 const fs = require('fs');
+const api = require('./routes/html.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-// Import custom middleware, "cLog"
-app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
@@ -19,29 +14,15 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-
-// GET HTML routes for homepage
-app.get('/api/notes', (req, res) => {
-  const notesData = fs.readFileSync('db/db.json', 'utf8');
-  const notes = JSON.parse(notesData);
-  res.json(notes);
-});
-
-// GET Route for index page
+// GET Route for homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// POST Route for notes page
-app.post('/api/notes', (req, res) => {
-  const notesData = fs.readFileSync('db/db.json', 'utf8');
-  const notes = JSON.parse(notesData);
-  const newNote = req.body;
-  newNote.id = uuid(); // You can use a package to generate a unique ID
-  notes.push(newNote);
-  fs.writeFileSync('db/db.json', JSON.stringify(notes));
-  res.json(newNote);
-});
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 app.listen(PORT, () =>
